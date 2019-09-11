0010f306bf9b168d6eb9648f629e97220dd970b3
Tyler Lindley <200382154@student.georgianc.on.ca>
2019-09-11 17:28:52 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoGameStore_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
